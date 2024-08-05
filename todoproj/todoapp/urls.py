818ff874ecde3083df02d1a47d4e63324b034de2
sHoNE <shonesamuel19@gmail.com>
2024-08-05 09:21:32 +0530

    added

---
+++ todoproj/todoapp/urls.py
@@ -0,0 +1,17 @@
+from django.templatetags import static
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.add,name='home.html'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.TaskDetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateView.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.TaskDetailview.as_view(),name='cbvdelete')
+
+
+
+   ]
